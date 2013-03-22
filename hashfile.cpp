54a5e9cb836c6506252c0d5415227ce973a77d14
David Wolf <d.wolf@live.at>
2013-03-22 18:50:09 +0100

    finished first version of HashFile

--- hashfile.cpp
+++ hashfile.cpp
@@ -11,7 +11,6 @@ HashFile::~HashFile (void)
 
 bool HashFile::load (const std::string& fileName, hashtable& table)
 {
-	ulong read = 0;
 	std::vector<hashtable::node>& hash = table.hsh_vector;
 	std::vector<size_t>& refTable = table.hsh_name;
 	FILE* file = fopen(fileName.c_str(), "r");
@@ -27,14 +26,12 @@ bool HashFile::load (const std::string& fileName, hashtable& table)
 	{
 		return false;
 	}
-	
 	fread((void*)&id[0], 1, 4, file);
 	
 	if (strcmp(&id[0], "hash"))
 	{
 		return false;
 	}
-	
 	fread((void*)&corpCount, sizeof(int), 1, file);
 	
 	for (register int i = 0; i < corpCount; i++)
@@ -90,19 +87,102 @@ bool HashFile::load (const std::string& fileName, hashtable& table)
 			shares.push_back(share);
 		}
 		// get key
-		unsigned key = Hash(shares[0].cont) % hashtable.size;
+		unsigned key = hashString(shares[0].cont) % table.size;
 		// set node
-		hashtable::node node = {shares, true};
+		table::node node = {shares, true};
 		// add to hash table
 		hash[key] = node;
 		// set reference
-		key = Hash(shares[0].name) % hashtable.size;
+		key = hashString(shares[0].name) % table.size;
 		// add to ref hash table
-		refTable[key] = (size_t)(Hash(shares[0].cont) % hashtable.size);
+		refTable[key] = (size_t)(hashString(shares[0].cont) % table.size);
 	}
 	return true;
 }
 
 bool HashFile::save (const std::string& fileName, const hashtable& table)
 {
+	unsigned size = table.size;
+	std::vector<hashtable::node> hash = table.hsh_vector;
+	std::vector<size_t> refTable = table.hsh_name;
+	FILE* file = fopen(fileName.c_str(), "r");
+	// write variables
+	int intValue = 0;
+	float floatValue = 0;
+	// file settings
+	int corpCount = 0;
+	int shareCount = 0;
+	// save indices
+	std::vector<unsigned> indices(0);
+	
+	if (!file)
+	{
+		return false;
+	}
+	fputs("hash", file);
+	
+	for (register unsigned i = 0; i < size; i++)
+	{
+		// check if hash is set
+		if (hash[i].set)
+		{
+			corpCount++;
+			indices.push_back(i);
+		}
+	}
+	fwrite((const void*)&corpCount, sizeof(int), 1, file);
+	
+	for (register unsigned i = 0; i < (unsigned)corpCount; i++)
+	{
+		std::vector<share_t> shares = hash[indices[i]].value;
+		int shareSize = (int)shares.size();
+		// write share count
+		fwrite((const void*)&shareSize, sizeof(int), 1, file);
+		// write shares
+		for (register unsigned j = 0; j < shareSize; j++)
+		{
+			// write name
+			intValue = (int)shares[j].name.size();
+			fwrite((const void*)&intValue, sizeof(int), 1, file);
+			for (register int k = 0; k < intValue; k++)
+			{
+				fputc(shares[j].name[k], file);
+			}
+			// write cont
+			intValue = shares[j].cont.size();
+			fwrite((const void*)&intValue, sizeof(int), 1, file);
+			for (register int k = 0; k < intValue; k++)
+			{
+				fputc(shares[j].cont[k], file);
+			}
+			// write day
+			intValue = shares[j].date.day;
+			fwrite((const void*)&intValue, sizeof(int), 1, file);
+			// write month
+			intValue = shares[j].date.month;
+			fwrite((const void*)&intValue, sizeof(int), 1, file);
+			// write year
+			intValue = shares[j].date.year;
+			fwrite((const void*)&intValue, sizeof(int), 1, file);
+			// write open
+			floatValue = shares[j].open;
+			fwrite((const void*)&floatValue, sizeof(float), 1, file);
+			// write high
+			floatValue = shares[j].high;
+			fwrite((const void*)&floatValue, sizeof(float), 1, file);
+			// write low
+			floatValue = shares[j].low;
+			fwrite((const void*)&floatValue, sizeof(float), 1, file);
+			// write close
+			floatValue = shares[j].close;
+			fwrite((const void*)&floatValue, sizeof(float), 1, file);
+			// write volume
+			floatValue = shares[j].volume;
+			fwrite((const void*)&floatValue, sizeof(float), 1, file);
+			// write adjClose
+			floatValue = shares[j].adjClose;
+			fwrite((const void*)&floatValue, sizeof(float), 1, file);
+		}
+	}
+	return true;
 }
